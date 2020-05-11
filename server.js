3606d9e007af0004ce9475555f3cd096d7d202ca
pattem1986 <rajasekharpattem@gmail.com>
2020-05-11 18:53:35 +0200

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { hostname } = require('os');
 const http = require('http');
-const message = 'Hello Cloud1236\n';
+const message = 'Hello Cloud\n';
 const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
